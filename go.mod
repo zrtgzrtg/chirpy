26112e54ddb31d37d5816f512a91efc0aaf9d0d6
zrtg <[thomas4stank@gmail.com]>
2025-09-29 04:08:21 +0200

    tried adding auth. Not working

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.25.0
 
 require (
 	github.com/alexedwards/argon2id v1.0.0 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.2
+	github.com/golang-jwt/jwt/v5 v5.3.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
--- internal/auth/main.go
+++ internal/auth/main.go
@@ -2,8 +2,11 @@ package auth
 
 import (
 	"errors"
+	"time"
 
 	"github.com/alexedwards/argon2id"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/google/uuid"
 )
 
 func HashPassword(password string) (string, error) {
@@ -29,4 +32,24 @@ func CheckPasswordHash(password, hash string) (bool, error) {
 		return false, errors.New("password doesnt match")
 	}
 	return true, nil
+}
+func MakeJWT(userId uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	time := time.Now()
+	expireTime := time.Add(expiresIn)
+	claims := jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  &jwt.NumericDate{time},
+		ExpiresAt: &jwt.NumericDate{expireTime},
+		Subject:   userId.String(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenStr, err := token.SignedString([]byte{})
+	if err != nil {
+		return "", err
+	}
+	return tokenStr, nil
+}
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	jwt.ParseWithClaims(tokenString)
 }
